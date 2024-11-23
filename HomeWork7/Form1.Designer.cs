5345481aa55d884cddcf33a28a349977ea4e5074
AndreaFoca <132893546+AndreaFocarelli01@users.noreply.github.com>
2024-11-23 15:50:51 +0100

    Add files via upload

---
+++ HomeWork7/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace HomeWork7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(37, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1034, 249);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(37, 328);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1034, 249);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(996, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(98, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(181, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(329, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Number of drawings";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Intervals";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(329, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Samples";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 589);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HomeWork7/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics theoretical = pictureBox1.CreateGraphics();
+            theoretical.Clear(Color.White);
+
+            Graphics empirical = pictureBox2.CreateGraphics();
+            empirical.Clear(Color.White);
+
+            //data 
+            int m = int.Parse(textBox1.Text);           //number of samples
+            int n = int.Parse(textBox3.Text);           //sample size
+            int interval = int.Parse(textBox2.Text);    //intervals size
+
+            float width = pictureBox1.Width;
+            float height = pictureBox1.Height;
+
+            double[] prob = GenerateProbabilities(interval);
+            double theoreticalMean = RecursiveMean(prob, interval - 1, 0, 0);
+            double theoreticalVariance = RecursiveVariance(prob, interval - 1, theoreticalMean, 0);
+
+
+            List<double> samplingMean = new List<double>();
+            Random rand = new Random();
+
+            for(int i = 0; i < m; i++)
+            {
+                samplingMean.Add(GenerateSampleMean(prob, n, interval, rand));
+            }
+
+            int[] empiricalDistribution = new int[interval];
+            foreach (double mean in samplingMean)
+            {
+                empiricalDistribution[(int)mean]++;
+            }
+            double samplingMeanAverage = samplingMean.Average();
+            double samplingMeanVariance = samplingMean.Select(x => Math.Pow(x - samplingMeanAverage, 2)).Average();
+
+            DrawHistogramForMeans(empirical, samplingMean, width, height, new Pen(Color.Green),
+                samplingMeanAverage, samplingMeanVariance, "Distribution of Sample Means");
+
+            DrawHistogramTheoretical(theoretical, empiricalDistribution, m, interval, height, width, new Pen(Color.Blue),
+                theoreticalMean, theoreticalVariance);
+        }
+
+        private double[] GenerateProbabilities(int intervals)
+        {
+            double[] probabilities = new double[intervals];
+            Random random = new Random();
+            double sum = 0;
+
+            for (int i = 0; i < intervals - 1; i++)
+            {
+                double remaining = 1 - sum;
+                double prob = random.NextDouble() * remaining * 0.8;
+                probabilities[i] = prob;
+                sum += prob;
+            }
+            probabilities[intervals - 1] = 1 - sum; // Ensure total sums to 1
+
+            return probabilities;
+        }
+
+        private double GenerateSampleMean(double[] probabilities, int sampleSize, int intervals, Random random)
+        {
+            double sampleSum = 0;
+            for (int i = 0; i < sampleSize; i++)
+            {
+                double rand = random.NextDouble();
+                double cumulative = 0;
+                for (int j = 0; j < intervals; j++)
+                {
+                    cumulative += probabilities[j];
+                    if (rand <= cumulative)
+                    {
+                        sampleSum += j;
+                        break;
+                    }
+                }
+            }
+            return sampleSum / sampleSize;
+        }
+
+        private void DrawHistogramForMeans(Graphics histogram, List<double> sampleMeans, float width, float height, Pen pen,
+    double mean, double variance, string title)
+        {
+            int bins = 20; // Number of bins for the histogram
+            double min = sampleMeans.Min();
+            double max = sampleMeans.Max();
+            double binWidth = (max - min) / bins;
+
+            int[] frequencies = new int[bins];
+            foreach (var value in sampleMeans)
+            {
+                int bin = Math.Min((int)((value - min) / binWidth), bins - 1);
+                frequencies[bin]++;
+            }
+
+            float barWidth = (width / bins) - 2;
+            float maxFrequency = frequencies.Max();
+            histogram.Clear(Color.White);
+
+            for (int i = 0; i < bins; i++)
+            {
+                float barHeight = (float)(frequencies[i] / maxFrequency * height);
+                histogram.FillRectangle(pen.Brush, i * (barWidth + 2), height - barHeight, barWidth, barHeight);
+            }
+
+            DrawTitleAndLabels(histogram, bins, height, barWidth, title, mean, variance);
+        }
+
+        private void DrawHistogramTheoretical(Graphics g, int[] frequencies, int totalSamples, int intervals,
+    float height, float width, Pen pen, double mean, double variance)
+        {
+            // Calculate proportions from frequencies
+            double[] proportions = frequencies.Select(f => (double)f / totalSamples).ToArray();
+
+            // Determine the maximum proportion for scaling
+            double maxProportion = proportions.Max();
+
+            // Calculate the bar width, leaving small spacing between bars
+            float barWidth = (width / intervals) - 2;
+
+            // Clear the drawing area
+            g.Clear(Color.White);
+            pen.Color = Color.Blue;
+            pen.Width = 2;
+
+            // Draw histogram bars
+            for (int i = 0; i < intervals; i++)
+            {
+                // Scale bar height to fit within the available height
+                float barHeight = (float)(proportions[i] / maxProportion * height);
+
+                // Draw the bar
+                g.FillRectangle(pen.Brush,
+                    i * (barWidth + 2),      // X-position (with spacing)
+                    height - barHeight,      // Y-position (inverted coordinate system)
+                    barWidth,                // Bar width
+                    barHeight);              // Bar height
+
+                // Add text label above each bar to show the proportion as a percentage
+                using (Font font = new Font("Arial", 8))
+                {
+                    string valueText = $"{(proportions[i] * 100):F1}%";
+                    SizeF textSize = g.MeasureString(valueText, font);
+                    float textX = i * (barWidth + 2) + (barWidth - textSize.Width) / 2; // Centered text
+                    float textY = height - barHeight - textSize.Height - 2;             // Position above bar
+                    g.DrawString(valueText, font, Brushes.Black, textX, textY);
+                }
+            }
+        }
+
+
+        private double RecursiveMean(double[] probabilities, int index, int value, double currentMean)
+        {
+            if (index < 0) return currentMean; // Base case
+            return RecursiveMean(probabilities, index - 1, value + 1, currentMean + value * probabilities[index]);
+        }
+
+        private double RecursiveVariance(double[] probabilities, int index, double mean, double currentVariance)
+        {
+            if (index < 0) return currentVariance; // Base case
+            double deviation = index - mean;
+            return RecursiveVariance(probabilities, index - 1, mean, currentVariance + deviation * deviation * probabilities[index]);
+        }
+
+        private void DrawTitleAndLabels(Graphics g, int intervals, float height, float barWidth,
+    string title, double mean, double variance)
+        {
+            using (Pen axisPen = new Pen(Color.Black, 2))
+            {
+                // Draw the X-axis
+                g.DrawLine(axisPen, 0, height, intervals * (barWidth + 2), height);
+
+                // Draw the Y-axis
+                g.DrawLine(axisPen, 0, 0, 0, height);
+
+                // Draw Y-axis labels and tick marks
+                for (int i = 0; i <= 10; i++) // 10 divisions on the Y-axis
+                {
+                    float y = height * (1 - i / 10.0f); // Position of the tick mark
+                    g.DrawLine(axisPen, -5, y, 5, y); // Tick mark
+
+                    using (Font font = new Font("Arial", 8))
+                    {
+                        g.DrawString($"{i * 10}%", font, Brushes.Black, -30, y - 6);
+                    }
+                }
+
+                // Draw X-axis labels for bins
+                using (Font font = new Font("Arial", 8))
+                {
+                    for (int i = 0; i < intervals; i++)
+                    {
+                        float x = i * (barWidth + 2) + barWidth / 2 - 10;
+                        g.DrawString(i.ToString(), font, Brushes.Black, x, height + 5);
+                    }
+                }
+
+                // Draw the title and statistics
+                using (Font titleFont = new Font("Arial", 10, FontStyle.Bold))
+                {
+                    string[] titleLines = title.Split('\n');
+                    float y = 10; // Start drawing title at the top
+
+                    foreach (string line in titleLines)
+                    {
+                        g.DrawString(line, titleFont, Brushes.Black, intervals * (barWidth + 2) / 2 - 75, y);
+                        y += 20; // Space between lines
+                    }
+
+                    // Add mean and variance details below the title
+                    g.DrawString($"Mean: {mean:F2}, Variance: {variance:F2}", titleFont, Brushes.Black,
+                        intervals * (barWidth + 2) / 2 - 75, y);
+                }
+            }
+        }
+
+    }
+}
